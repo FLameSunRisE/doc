5870f29b807da1d2314b2b3b5a0394bca0bed582
FLameSunRisE <flamesunrises@gmail.com>
2022-05-17 15:25:44 +0800

    feat: export import demo2

---
+++ learn/fullstack/JSLab1/src/demo3_course.js
@@ -0,0 +1,6 @@
+const course = {
+  name: "React with Srpgin boot",
+  duration: 8,
+};
+
+export default course;
---
+++ learn/fullstack/JSLab1/src/demo3_main2.js
@@ -0,0 +1,5 @@
+import * as bundle from "./demo3_common";
+import c from "./demo3_course";
+console.log(`max size=${bundle.MAX_SIZE}`);
+bundle.default_header();
+console.log("course=", c);
