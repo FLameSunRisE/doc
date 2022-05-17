596d1ccd0a3137d392573682aee215afa4babd1b
FLameSunRisE <flamesunrises@gmail.com>
2022-05-17 13:13:03 +0800

    feat: spring boot init project

---
+++ learn/fullstack/BackendLab1/src/main/java/com/uuu/fullstack/BackendLab1/BackendLab1Application.java
@@ -0,0 +1,13 @@
+package com.uuu.fullstack.BackendLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLab1Application.class, args);
+	}
+
+}
---
+++ learn/fullstack/BackendLab1/src/test/java/com/uuu/fullstack/BackendLab1/BackendLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuu.fullstack.BackendLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
