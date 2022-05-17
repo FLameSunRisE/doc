dd738049fe14efca8deb1e7287a88ee2a080dc4e
FLameSunRisE <flamesunrises@gmail.com>
2022-05-17 15:11:22 +0800

    feat: export import demo

---
+++ learn/fullstack/JSLab1/src/demo3_common.js
@@ -0,0 +1,4 @@
+export const default_header = () => {
+  console.log("___this is a header___");
+};
+export const MAX_SIZE = 50;
---
+++ learn/fullstack/JSLab1/src/demo3_main1.js
@@ -0,0 +1,4 @@
+import { default_header, MAX_SIZE } from "./demo3_common";
+
+console.log(`max size=${MAX_SIZE}`);
+default_header();
