5565cbf3f63ae7913b2e83cac8327053bd2c68a3
FLameSunRisE <flamesunrises@gmail.com>
2022-05-18 11:06:21 +0800

    feat: react import component

--- learn/react/react_lab1/src/App.js
+++ learn/react/react_lab1/src/App.js
@@ -1,19 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hello World</h1>
-    //   <h2>React Programming</h2>
-    // </div>
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>React Programming</h2>
+      <Dashboard></Dashboard>
+      <Dashboard></Dashboard>
+      <Dashboard></Dashboard>
+    </div>
     //React.createElement('div',null,"h1","Hi, this is react programming")
-    React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Hi"),
-      React.createElement("h1", null, "Hi I am Mark")
-    )
+    // React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi"),
+    //   React.createElement("h1", null, "Hi I am Mark")
+    // )
   );
 }
 
