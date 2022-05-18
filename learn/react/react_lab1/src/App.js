5bc8d6ae04f2ce80e0909e71f3d5c6b3ca4fa704
FLameSunRisE <flamesunrises@gmail.com>
2022-05-18 13:38:51 +0800

    feat: react state

--- learn/react/react_lab1/src/App.js
+++ learn/react/react_lab1/src/App.js
@@ -4,6 +4,14 @@ import Dashboard from "./components/Dashboard.js";
 import Person from "./Person/Person";
 import Pet from "./Pet/Pet";
 class App extends React.Component {
+  state = {
+    persons: [
+      { name: "Mark", age: 44 },
+      { name: "James", age: 34 },
+      { name: "Tim", age: 49 },
+      { name: "Kevin", age: 54 },
+    ],
+  };
   changeNameHandler() {
     console.log("button clicked!!");
   }
@@ -11,14 +19,24 @@ class App extends React.Component {
     return (
       <div className="App">
         <button onClick={this.changeNameHandler}>Change</button>
-        <Person name="Mark" age="44" />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
         <Pet name="king" specie="cat" />
-        <Person name="James" age="37" />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
         <Pet name="oven" specie="dog" />
-        <Person name="Tim">Team Leader</Person>
-        <Person name="Kevin" age="50" />
-        <Person name="Abby" age="35" />
-        <Person name="Rolin" age="60" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
       </div>
     );
   }
