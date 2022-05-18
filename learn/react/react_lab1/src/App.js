3f92bf3ae77ec75004e22a0b4a7e18458219a8e7
FLameSunRisE <flamesunrises@gmail.com>
2022-05-18 13:31:01 +0800

    feat: method

--- learn/react/react_lab1/src/App.js
+++ learn/react/react_lab1/src/App.js
@@ -3,19 +3,25 @@ import React from "react";
 import Dashboard from "./components/Dashboard.js";
 import Person from "./Person/Person";
 import Pet from "./Pet/Pet";
-function App() {
-  return (
-    <div className="App">
-      <Person name="Mark" age="44" />
-      <Pet name="king" specie="cat" />
-      <Person name="James" age="37" />
-      <Pet name="oven" specie="dog" />
-      <Person name="Tim">Team Leader</Person>
-      <Person name="Kevin" age="50" />
-      <Person name="Abby" age="35" />
-      <Person name="Rolin" age="60" />
-    </div>
-  );
+class App extends React.Component {
+  changeNameHandler() {
+    console.log("button clicked!!");
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.changeNameHandler}>Change</button>
+        <Person name="Mark" age="44" />
+        <Pet name="king" specie="cat" />
+        <Person name="James" age="37" />
+        <Pet name="oven" specie="dog" />
+        <Person name="Tim">Team Leader</Person>
+        <Person name="Kevin" age="50" />
+        <Person name="Abby" age="35" />
+        <Person name="Rolin" age="60" />
+      </div>
+    );
+  }
 }
 
 export default App;
