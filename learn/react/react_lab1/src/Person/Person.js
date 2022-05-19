277efb9890c250bf2f5e71c695d3b701eb05682b
FLameSunRisE <flamesunrises@gmail.com>
2022-05-19 11:17:38 +0800

    feat: react style component

--- learn/react/react_lab1/src/Person/Person.js
+++ learn/react/react_lab1/src/Person/Person.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
-import "./Person.css";
+// import "./Person.css";
+import styled from "styled-components";
 const Person = (props) => {
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 3px solid #ffff00;
+    box-shadow: 0 2px 3px #ffc066;
+    padding: 16px;
+    text-align: center;
+    background-color: #c0ffee;
+    @media (min-width: 200px) {
+      width: 400px;
+    }
+  `;
   const [stateString, changeString] = useState({
     publisher: "Marvel Studio",
     studio: "Disney",
@@ -15,7 +28,7 @@ const Person = (props) => {
     });
   };
   return (
-    <div className="Person">
+    <StyledDiv className="Person">
       <h1>{stateString.publisher}</h1>
       <button onClick={switchVenderHandler}>Change!</button>
       <button onClick={showStatus}>show status</button>
@@ -24,7 +37,7 @@ const Person = (props) => {
         am {props.age} years old, I am the {Math.floor(Math.random() * 10)}th
         generation
       </p>
-    </div>
+    </StyledDiv>
   );
 };
 
