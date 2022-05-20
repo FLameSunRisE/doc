ee16a85fd6e7c2668afe544272129fe268d89a29
FLameSunRisE <flamesunrises@gmail.com>
2022-05-20 14:19:56 +0800

    feat : CrossOrigin

--- learn/fullstack/BackendLab1/src/main/java/com/uuu/fullstack/BackendLab1/controllers/ProjectController.java
+++ learn/fullstack/BackendLab1/src/main/java/com/uuu/fullstack/BackendLab1/controllers/ProjectController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 @Slf4j
 public class ProjectController {
     @Autowired
